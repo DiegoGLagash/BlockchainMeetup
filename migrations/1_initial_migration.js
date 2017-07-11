a1112567be9d5fd9055b0829dced48524687dc3e
diegoglagash <diegog.lagash@gmail.com>
2017-07-11 10:22:20 -0300

    Update to run on testrpc first.

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,6 +1,7 @@
 var Migrations = artifacts.require("./Migrations.sol");
 
 module.exports = function(deployer) {
-  web3.personal.unlockAccount(web3.eth.accounts[0], "", 60*1000)
+  // Uncomment when using DevNet, not supported on TestRPC
+  // web3.personal.unlockAccount(web3.eth.accounts[0], "", 60*1000)
   deployer.deploy(Migrations);
 };
